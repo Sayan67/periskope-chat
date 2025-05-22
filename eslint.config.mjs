04e1c5d05cca389591a462f23ff18615a031f5a6
Sayan67 <sayan.das.21@aot.edu.in>
2025-05-22 13:21:50 +0530

    Lint rule: turned off lint 2 rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,12 +12,14 @@ const compat = new FlatCompat({
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   ...compat.config({
-    extends:["next"],
-    rules:{
+    extends: ["next"],
+    rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
-    }
-  })
+      "react-hooks/rules-of-hooks": "off",
+      "react/no-unescaped-entities" : "off",
+    },
+  }),
 ];
 
 export default eslintConfig;
